5d106c62cefd829e08b4a25552960cfc2af54ded
yugan2005Dell17 <ygan2010@gmail.com>
2015-04-16 20:52:55 -0700

    Start Github+Eclipse project
    Signed-off-by: yugan2005Dell17 <ygan2010@gmail.com>

---
+++ HelloWorld/src/edu/java/leetcode/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.java.leetcode;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ HelloWorld/src/edu/java/leetcode/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author YG
+ *
+ */
+package edu.java.leetcode;
