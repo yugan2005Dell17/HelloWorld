004052dfab954dccdbba0825dcc6a7f62caa2181
yugan2005Dell17 <ygan2010@gmail.com>
2015-04-18 12:40:06 -0700

    Add some stuff for the Udemy Git exercise

---
+++ HelloWorld/src/edu/java/leetcode/GitExample.java
@@ -0,0 +1,9 @@
+package edu.java.leetcode
+
+public class GitExample {
+	void public static void main(String[] args) {
+
+		System.out.println("This is udemy git example excerise");
+		
+	}
+} 
--- HelloWorld/src/edu/java/leetcode/HelloWorld.java
+++ HelloWorld/src/edu/java/leetcode/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Want to make it a little bit longer");
+		System.out.println("added the 3rd line");
 
 	}
 
